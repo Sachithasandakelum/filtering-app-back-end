57265285dfa232dc3358cd1bd2bc8d12555ae093
Sachitha Abesinghe <sachithasandakelumsl@gmail.com>
2023-12-28 01:01:41 +0530

    Initial commit

---
+++ src/main/java/lk/ijse/dep11/app/api/CustomerHttpController.java
@@ -0,0 +1,10 @@
+package lk.ijse.dep11.app.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/customers")
+public class CustomerHttpController {
+
+}
