cab0a86e49d63661ee94da40fb10e6ef327a8a79
Sachitha Abesinghe <sachithasandakelumsl@gmail.com>
2023-12-28 17:11:12 +0530

    Add application.properties file

--- src/main/java/lk/ijse/dep11/app/api/CustomerHttpController.java
+++ src/main/java/lk/ijse/dep11/app/api/CustomerHttpController.java
@@ -1,5 +1,7 @@
 package lk.ijse.dep11.app.api;
 
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
